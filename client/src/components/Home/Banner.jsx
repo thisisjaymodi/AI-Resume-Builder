001040481ba4430661094287dfe25e7761bc196c
thisisjaymodi <jaymodi35@gmail.com>
2026-02-08 16:13:18 -0500

    added a bug

--- client/src/components/Home/Banner.jsx
+++ client/src/components/Home/Banner.jsx
@@ -5,9 +5,9 @@ const Banner = () => {
     <div className="w-full py-2.5 font-medium text-sm text-green-800 text-center bg-gradient-to-r from-[#ABFF7E] to-[#FDFEFF]">
       <p>
         <span className="px-3 py-1 rounded-lg text-white bg-green-600 mr-2">
-          New
+          New Noew
         </span>
-        AI Feature Added
+        AI Feature Added 
       </p>
     </div>
   );
