26e6e97f353414ac8c26b797554e20c4a761e30b
thisisjaymodi <jaymodi35@gmail.com>
2026-02-08 17:04:53 -0500

    v2 commit

--- client/src/components/Home/Banner.jsx
+++ client/src/components/Home/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className="w-full py-2.5 font-medium text-sm text-green-800 text-center bg-gradient-to-r from-[#ABFF7E] to-[#FDFEFF]">
       <p>
         <span className="px-3 py-1 rounded-lg text-white bg-green-600 mr-2">
-          New New
+          New V2
         </span>
         AI Feature Added 
       </p>
